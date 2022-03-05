6770b780828366fcf1f51be6eb92ab56770f1796
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-05 13:14:56 +0300

    function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getAkanName(){
+    var yearOfbirth=document.getElementById("year-input").value;
+    var monthOfbirth=number.getElementById("month-input").value;
+    var dayOfbirth=number.getElementById("day-input").value;
+    var gender=document.getElementsByName("gender");
+}
+
