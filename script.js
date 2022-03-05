c5ca4b1c19bd01e91c12d6dedd5b2617c60ec4b6
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-05 13:27:24 +0300

    call second function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function getAkanName(){
-    var yearOfbirth=document.getElementById("year-input").value;
-    var monthOfbirth=number.getElementById("month-input").value;
-    var dayOfbirth=number.getElementById("day-input").value;
+    var yearOfBirth=document.getElementById("year-input").value;
+    var monthOfBirth=number.getElementById("month-input").value;
+    var dayOfBirth=number.getElementById("day-input").value;
     var gender=document.getElementsByName("gender");
 }
-
+function getGender(){
+    for(let gender of genders){
+        if(gender.checked){
+            return gender.value;
+        }
+    }
+}
+var myGenderValue= getGender();
+function monthValidator(){
+    if(monthOfBirth < 1 || monthOfBirth > 12){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function dayValidator(){
+    if(monthOfBirth === 2 && Number(yearOfBirth)%4===0)
+}
